f4a1194fdc61f776282fc5bfacc720af8d41fc59
Nikolaj Potashnikoff <34554890+fiddlededee@users.noreply.github.com>
2023-09-07 23:21:52 +0300

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -1,7 +1,7 @@
 ./gradlew run
-#docker run --rm -v $PWD:/documents -w /documents/output curs/asciidoctor-od \
-#  asciidoctor flow-docops.adoc
-#docker run --rm -v $PWD:/documents -w /documents/output curs/asciidoctor-od \
-#  a-od flow-docops.adoc pdf ../src/template.fodt ../src/custom.rb
-#docker run --rm -v $PWD:/documents -w /documents/output curs/asciidoctor-od \
-#  a-od flow-docops.adoc docx ../src/template.fodt ../src/custom.rb
+docker run --rm -v $PWD:/documents -w /documents/output curs/asciidoctor-od \
+  asciidoctor flow-docops.adoc
+docker run --rm -v $PWD:/documents -w /documents/output curs/asciidoctor-od \
+  a-od flow-docops.adoc pdf ../src/template.fodt ../src/custom.rb
+docker run --rm -v $PWD:/documents -w /documents/output curs/asciidoctor-od \
+  a-od flow-docops.adoc docx ../src/template.fodt ../src/custom.rb
